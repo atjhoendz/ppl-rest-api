d84c6d31df8555ba2379b5549511a013b7e8c2eb
atjhoendz <achunarmando@gmail.com>
2020-10-21 22:03:05 +0700

    move route to one index

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var router = require('./routes/index');
 
 var app = express();
 
@@ -12,9 +11,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/', router);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 404,
+    message: 'Route Not Found...'
+  })
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sendResponse = require('../utils/sendResponse')
+
+const user = require('./user')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return sendResponse(res, true, 200, {}, 'API running successfuly')
 });
+
+router.use('/user', user)
 
 module.exports = router;
